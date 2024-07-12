c6b8751d39f9b88b2ea4bbda240e5095e27c8a4e
goku483 <brandow.hidalgo@gmail.com>
2024-07-12 07:46:42 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,20 @@
+var btnno = document.getElementById("btnno");
+var btnyes = document.getElementById("btnyes");
+
+btnno.addEventListener("mouseenter", () => {
+    var pos = coordenadas();
+    btnno.style.top = `${pos.posy}px`;
+    btnno.style.left = `${pos.posx}px`;
+});
+
+var coordenadas = () => {
+    var ancho = innerWidth - 150;
+    var alto = innerHeight - 50;
+  var posx = Math.floor(Math.random() * ancho);
+  var posy = Math.floor(Math.random() * alto);
+    return { posx, posy };
+};
+
+btnyes.addEventListener("click", () => {
+    alert("Yo tambien me siento feliz, Nunca pense en que llegariamos a cumplir un mes pero ahora que lo cumplimos estoy seguro que seremos capaces de cumplir muchos mas, Eres el foco de luz que ilumino mi corazon cuando pense que solo habria oscuridad, la persona que me hace querer mejorar dia a dia para ser esa persona que este para ti cuando me necesites. TE AMO MUCHO de parte de tu Gatito para mi mujer ❤.");location.href="file:///C:/Users/Brandow%20A.%20Hidalgo/OneDrive/Documentos/Primer_Mes/pagina-fotos/pagina.html"
+});
